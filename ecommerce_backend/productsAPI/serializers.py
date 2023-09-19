41255fa1da85ef700be36622c15821c18dd297a0
tanimsk <sktanim5800@gmail.com>
2023-09-19 21:17:03 +0600

    ..

--- ecommerce_backend/productsAPI/serializers.py
+++ ecommerce_backend/productsAPI/serializers.py
@@ -18,14 +18,8 @@ class FeaturedProductSerializer(serializers.ModelSerializer):
     product_id = serializers.IntegerField(source="product.id", read_only=True)
     category_id = serializers.IntegerField(source="product.category.id", read_only=True)
     name = serializers.CharField(source="product.name", read_only=True)
-    image_url = serializers.SerializerMethodField(read_only=True)
+    image_url = serializers.ListField(source="product.images", read_only=True)
     price = serializers.IntegerField(source="product.price_bdt", read_only=True)
-
-    def get_image_url(self, obj):
-        first_image = obj.images[0]
-        if first_image:
-            return first_image
-        return None
 
     class Meta:
         fields = (
