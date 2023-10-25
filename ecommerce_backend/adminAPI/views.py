077b0a6fe70c48672fa840cb67ac4619a0f788eb
tanimsk <sktanim5800@gmail.com>
2023-10-25 23:51:37 +0600

    order

--- ecommerce_backend/adminAPI/views.py
+++ ecommerce_backend/adminAPI/views.py
@@ -38,9 +38,9 @@ from rest_framework.pagination import PageNumberPagination
 
 # Pagination Config
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 6
+    page_size = 10
     page_size_query_param = "page_size"
-    max_page_size = 10
+    max_page_size = 12
     page_query_param = "p"
 
 
